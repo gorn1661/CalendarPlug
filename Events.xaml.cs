06bb6782610542cebde9d76483845b6a759cf561
gorn1661 <mikolaj116@op.pl>
2017-03-31 15:57:38 +0200

    Add files via upload

---
+++ Events.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CalendarPlug
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Events.xaml
+    /// </summary>
+    public partial class Events : Window
+    {
+        public Events(int day, int month, int year)
+        {
+            InitializeComponent();
+            this.year.Content += year.ToString();
+            this.month.Content += month.ToString();
+            this.day.Content += day.ToString();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalendarPlug
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string[] splitedDate = new string[1];
+            splitedDate = calPlug.SelectedDate.ToString().Split(new char[] { '-', ' '});
+
+            Events es = new Events(int.Parse(splitedDate[0]), calPlug.DisplayDate.Month, calPlug.DisplayDate.Year);
+            es.Show();
+        }
+    }
+}
